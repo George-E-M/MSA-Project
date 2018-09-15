371205154dbbdf4e480599a55998997cd09eb7e6
George <georgeedwinmcerlean@gmail.com>
2018-09-15 13:54:23 +1200

    fixed api and added css

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
-import './App.css';
+import './css/styles.css';
 
 export const WEATHER_KEY = 'c42209f7f1cc21a210304e6a21d108ae';
 
 interface IState {
   inputText: any,
+  location: any,
   weatherDescription: any,
-  errorMessage: any,
-  temperature: any
+  temperature: any,
+  windSpeed: any,
+  errorMessage: any
 }
 
 class App extends React.Component<{}, IState> {
@@ -17,9 +19,11 @@ class App extends React.Component<{}, IState> {
 
     this.state = {
       inputText: '',
+      location: undefined,
       weatherDescription: undefined,
-      errorMessage: undefined,
       temperature: undefined,
+      windSpeed: undefined,
+      errorMessage: undefined
     };
 
     this.handleTextChange = this.handleTextChange.bind(this);
@@ -31,45 +35,51 @@ class App extends React.Component<{}, IState> {
   }
 
   public handleTextSubmit(event: any) {
-    this.query(this.state.inputText);
-  }
-
-  public query(cityName: any) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${WEATHER_KEY}`, {})
-    .then((response: any) => {
+    event.preventDefault();
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.inputText}&appid=${WEATHER_KEY}`, {})
+    .then((response : any) => {
       if (!response.ok) {
-        this.setState({errorMessage: 'no response'})
+        this.setState({
+          location: undefined,
+          weatherDescription: undefined,
+          temperature: undefined,
+          windSpeed: undefined,
+          errorMessage: "Could not find city"
+        })
       } else {
         response.json().then((data:any) =>
          this.setState({
+           location: data.name + ", " + data.sys.country,
            weatherDescription: data.weather[0].description,
            temperature: data.main.temp,
+           windSpeed: data.wind.speed,
            errorMessage: ''
           })
        )}
     })
   }
 
+
   public render() {
     return (
-      <div>
-        <div>
+      <div className="centreText">
+        <div className="titles">
           <h1>Weather App </h1>
           <p> Enter the name of the city you want to find the weather for </p>
         </div>
-        <div>
+        <div className="form">
          <form onSubmit={this.handleTextSubmit}>
-          <label>
-            City:
-            <input type="text" value={this.state.inputText} onChange={this.handleTextChange} placeholder="Please enter the name of a city" />
-          </label>
-          <input type="submit" value="Find Weather" />
+            <input type="text" value={this.state.inputText} onChange={this.handleTextChange} 
+            placeholder="Please enter the name of a city" style={{width: "300px"}} />
+            <button>Find Weather</button>
          </form>
         </div>
-        <div>
-          <p> Weather: {this.state.weatherDescription} </p>
-          <p> Temperature: {this.state.temperature} </p>
-          <p> Error: {this.state.errorMessage} </p>
+        <div className="information">
+          {this.state.errorMessage && <p> Error: {this.state.errorMessage} </p>}
+          {this.state.location && <p> Location: {this.state.location} </p>}
+          {this.state.weatherDescription && <p> Weather: {this.state.weatherDescription} </p>}
+          {this.state.temperature && <p> Temperature: {Math.round(this.state.temperature - 273.15)} °C </p>}
+          {this.state.windSpeed && <p> Wind Speed: {this.state.windSpeed} km/h </p>}
         </div>
       </div>
     );
